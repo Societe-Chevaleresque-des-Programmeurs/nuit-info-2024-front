9a8630c75bbd365995944e4e7eb0e73a010ac91e
Guilhem Fauré <pro@gfaure.eu>
2024-12-06 06:12:17 +0100

    feat: fish fryer ok, output not used

--- script.js
+++ script.js
@@ -1,129 +1,187 @@
-function showBone(section) {
-  section.innerHTML = ''; // Clear the current content
+let fishCooker = {
+  increment: 0,
+  width: 0,
+  intervalId: null,
+  stoped: false,
+  cooking: -1,
+}
 
-  const title = document.createElement('h3');
-  title.textContent = 'Le corail, le "squelette" de l’océan';
-  const image = document.createElement('img');
-  image.src = 'ocean-organs/bone.webp';
-  image.alt = 'Récif coralien';
+function showBone(section) {
+  section.innerHTML = '' // Clear the current content
+
+  const title = document.createElement('h3')
+  title.textContent = 'Le corail, le "squelette" de l’océan'
+  const image = document.createElement('img')
+  image.src = 'ocean-organs/bone.webp'
+  image.alt = 'Récif coralien'
 
   section.appendChild(title)
   section.appendChild(image)
 }
 
-function showBrain(section) {
-  section.innerHTML = ''; // Clear the current content
+function startProgress() {
+  fishCooker.width = 0; // Réinitialise la barre si elle est pleine
+  fishCooker.progressBar.style.width = '0%';
+  fishCooker.fish.src = 'clicker/fish.webp';
+  fishCooker.fish.classList = 'fish';
+  fishCooker.progressBar.style.background = "linear-gradient(to right, #4caf50, #81c784)";
+  const speed = Math.random() * (80 - 10) + 10; // Vitesse aléatoire (10 ms à 50 ms)
+  fishCooker.intervalId = setInterval(() => {
+    fishCooker.width += 1; // Augmente la largeur de 1%
+    fishCooker.progressBar.style.width = fishCooker.width + '%';
+    if (fishCooker.width > 110) {
+      fishCooker.progressBar.style.background = "linear-gradient(to right, #4caf50, red)";
+    }
+  }, speed);
+}
 
-  const title = document.createElement('h3');
-  title.textContent = 'Poisson pané';
-  const image = document.createElement('img');
-  image.src = 'ocean-organs/brain.webp';
-  image.alt = 'Gulf stream';
+function stopProgress() {
+  clearInterval(fishCooker.intervalId); // Arrête l'animation
+  if (fishCooker.width < 90) {
+    fishCooker.cooking = -1;
+  } else if (fishCooker.width > 110) {
+    fishCooker.cooking = 1;
+    fishCooker.fish.src = 'clicker/coal.webp';
+  } else {
+    fishCooker.increment++;
+    fishCooker.fish.src = 'clicker/fried.webp';
+    fishCooker.fish.alt = 'Poisson pané';
+    fishCooker.cooking = 0;
+  }
+  fishCooker.fish.classList = '';
+}
+
+function showBrain(section) { // Cookie clicker captcha
+  section.innerHTML = '' // Clear the current content
+
+  const title = document.createElement('h3')
+  title.textContent = 'Prouvez que vous êtes humain en le faisant frire la bonne durée'
+  const fish = document.createElement('img')
+  fish.src = 'clicker/fish.webp'
+  fish.alt = 'Un poisson cru'
+  fish.id = 'fish'
+  const fryer = document.createElement('img')
+  fryer.src = 'clicker/fryer.webp'
+  fryer.alt = 'Gulf stream'
+  const progressContain = document.createElement('div')
+  progressContain.id = 'progress-container'
+  const progressBar = document.createElement('div')
+  progressBar.id = 'progress-bar'
 
   section.appendChild(title)
-  section.appendChild(image)
+  section.appendChild(fish)
+  section.appendChild(fryer)
+  section.appendChild(progressContain)
+  progressContain.appendChild(progressBar)
+
+  fishCooker.fish = fish
+  fishCooker.progressBar = progressBar
+
+  progressContain.addEventListener('mousedown', startProgress);
+  progressContain.addEventListener('mouseup', stopProgress);
+  progressContain.addEventListener('mouseleave', stopProgress);
 }
 
 function showHeart(section) {
-  section.innerHTML = ''; // Clear the current content
+  section.innerHTML = '' // Clear the current content
 
-  const title = document.createElement('h3');
-  title.textContent = 'Les courants, le "cœur" de l’océan';
-  const image = document.createElement('img');
-  image.src = 'ocean-organs/heart.webp';
-  image.alt = 'Gulf stream';
+  const title = document.createElement('h3')
+  title.textContent = 'Les courants, le "cœur" de l’océan'
+  const image = document.createElement('img')
+  image.src = 'ocean-organs/heart.webp'
+  image.alt = 'Gulf stream'
 
   section.appendChild(title)
   section.appendChild(image)
 }
 
 function showKidneys(section) {
-  section.innerHTML = ''; // Clear the current content
+  section.innerHTML = '' // Clear the current content
 
-  const title = document.createElement('h3');
-  title.textContent = 'Les poissons nettoyeurs, les "reins" de l’océan';
-  const image = document.createElement('img');
-  image.src = 'ocean-organs/kidneys.webp';
-  image.alt = 'Poisson nettoyeur';
+  const title = document.createElement('h3')
+  title.textContent = 'Les poissons nettoyeurs, les "reins" de l’océan'
+  const image = document.createElement('img')
+  image.src = 'ocean-organs/kidneys.webp'
+  image.alt = 'Poisson nettoyeur'
 
   section.appendChild(title)
   section.appendChild(image)
 }
 
 function showLungs(section) {
-  section.innerHTML = ''; // Clear the current content
+  section.innerHTML = '' // Clear the current content
 
-  const title = document.createElement('h3');
-  title.textContent = 'Les phytoplanctons, les "poumons" des océans';
-  const image = document.createElement('img');
-  image.src = 'ocean-organs/lungs.webp';
-  image.alt = 'Phytoplanctons';
+  const title = document.createElement('h3')
+  title.textContent = 'Les phytoplanctons, les "poumons" des océans'
+  const image = document.createElement('img')
+  image.src = 'ocean-organs/lungs.webp'
+  image.alt = 'Phytoplanctons'
 
   section.appendChild(title)
   section.appendChild(image)
 }
 
 function showTumor(section) {
-  section.innerHTML = ''; // Clear the current content
+  section.innerHTML = '' // Clear the current content
 
-  const title = document.createElement('h3');
-  title.textContent = 'Le continent plastique, la tumeur de l’océan';
-  const image = document.createElement('img');
-  image.src = 'ocean-organs/tumor.webp';
-  image.alt = 'Continent plastique';
+  const title = document.createElement('h3')
+  title.textContent = 'Le continent plastique, la tumeur de l’océan'
+  const image = document.createElement('img')
+  image.src = 'ocean-organs/tumor.webp'
+  image.alt = 'Continent plastique'
 
   section.appendChild(title)
   section.appendChild(image)
 }
 
 function showReproductive(section) {
-  section.innerHTML = ''; // Clear the current content
+  section.innerHTML = '' // Clear the current content
 
-  const title = document.createElement('h3');
-  title.textContent = 'Comme les tortues, de nombreuses espèces ont du mal à se reproduire';
-  const image = document.createElement('img');
-  image.src = 'ocean-organs/reproductive.webp';
-  image.alt = 'Bébés tortues';
+  const title = document.createElement('h3')
+  title.textContent = 'Comme les tortues, de nombreuses espèces ont du mal à se reproduire'
+  const image = document.createElement('img')
+  image.src = 'ocean-organs/reproductive.webp'
+  image.alt = 'Bébés tortues'
 
   section.appendChild(title)
   section.appendChild(image)
 }
 
 function listenMapAreas(mapName) {
-  const map = document.querySelector('map[name=' + mapName + ']');
-  const areas = map.querySelectorAll('area');
+  const map = document.querySelector('map[name=' + mapName + ']')
+  const areas = map.querySelectorAll('area')
   areas.forEach(a => {
     a.addEventListener('click', event => {
       event.preventDefault()
-      const title = event.target.getAttribute('title');
-      console.log('Opening organ: ' + title);
-      const organSection = document.querySelector('.ocean-organs');
+      const title = event.target.getAttribute('title')
+      console.log('Opening organ: ' + title)
+      const organSection = document.querySelector('.ocean-organs')
       switch (title) {
         case 'bone':
-          showBone(organSection);
-          break;
+          showBone(organSection)
+          break
         case 'brain':
-          showBrain(organSection);
-          break;
+          showBrain(organSection)
+          break
         case 'heart':
-          showHeart(organSection);
-          break;
+          showHeart(organSection)
+          break
         case 'kidneys':
-          showKidneys(organSection);
-          break;
+          showKidneys(organSection)
+          break
         case 'lungs':
-          showLungs(organSection);
-          break;
+          showLungs(organSection)
+          break
         case 'lungs':
-          showLungs(organSection);
-          break;
+          showLungs(organSection)
+          break
         case 'tumor':
-          showTumor(organSection);
-          break;
+          showTumor(organSection)
+          break
         case 'reproductive':
-          showReproductive(organSection);
-          break;
+          showReproductive(organSection)
+          break
       }
-    });
-  });
+    })
+  })
 }
